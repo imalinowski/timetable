921d768432b026a149fbd3427778989c07039592
Ilya Malinowski <malinowski221106@gmail.com>
2021-11-15 00:45:41 +0300

    init backend

---
+++ backend/src/main/kotlin/com/malinowski/Application.kt
@@ -0,0 +1,11 @@
+package com.malinowski
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.malinowski.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
