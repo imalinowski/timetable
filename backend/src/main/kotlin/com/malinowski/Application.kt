fe53155a21a9d0bf7837f87a011506e227ca333d
Ilya Malinowski <malinowski221106@gmail.com>
2021-11-15 01:12:54 +0300

    sample backend api

--- backend/src/main/kotlin/com/malinowski/Application.kt
+++ backend/src/main/kotlin/com/malinowski/Application.kt
@@ -1,11 +1,24 @@
 package com.malinowski
 
+import com.jetbrains.handson.httpapi.routes.registerCustomerRoutes
+import com.jetbrains.handson.httpapi.routes.registerOrderRoutes
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.malinowski.plugins.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
+        module()
     }.start(wait = true)
 }
+
+fun Application.module(testing: Boolean = true) {
+    install(ContentNegotiation) {
+        json()
+    }
+    registerCustomerRoutes()
+    registerOrderRoutes()
+}
+
