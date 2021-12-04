2d7e365d4bbfbcc7e195206111707764a2138c6a
Ilya Malinowski <malinowski221106@gmail.com>
2021-12-04 14:24:39 +0300

    authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import * as ReactDOM from "react-dom";
 import React from "react";
+import {Auth0Provider, useAuth0} from "@auth0/auth0-react";
 import './index.css'
 import {Game} from "./components/game"
 import axios from "axios";
@@ -10,9 +11,47 @@ const test = async () => {
     console.log("response from server > " + response.data)
 }
 test()
+const LoginButton = () => {
+    const {loginWithRedirect} = useAuth0();
+    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+const LogoutButton = () => {
+    const {logout} = useAuth0();
+    return <button onClick={() => logout({returnTo: window.location.origin})}>Log Out</button>;
+};
+
+const Profile = () => {
+    const {user, isAuthenticated, isLoading} = useAuth0();
+
+    if (isLoading) {
+        return <div>Loading ...</div>;
+    }
+
+    if (!isAuthenticated) {
+        return <div> Authenticate ...</div>;
+    }
+
+    return (isAuthenticated && (
+            <div>
+                <img src={user.picture} alt={user.name}/>
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+            </div>
+        )
+    );
+};
 
 ReactDOM.render(
     <Game/>,
-    document.getElementById('root')
+    <Auth0Provider
+        domain="dev-x8crmkcg.us.auth0.com"
+        clientId="QdQZrrvlvbuAmPNih9xLiXA3SW3oDQoE"
+        redirectUri={window.location.origin}>
+        <LoginButton/>,
+        <LogoutButton/>,
+        <Profile/>,
+    </Auth0Provider>,
+    document.getElementById("root")
 );
 
